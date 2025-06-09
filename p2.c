bcbe767e487faf8d41cba10c9e979cac695c7cfa
Kishan Mallick <kishanmallick203@gmail.com>
2025-06-09 16:39:17 +0530

    code

---
+++ p2.c
@@ -0,0 +1,23 @@
+//Learning Datatype
+
+//https://github.com/kishan792/c-Code-10AM
+#include<stdio.h>
+void main()
+{
+    //datatype variableName = Value;
+    
+    /*
+        Multi 
+            Line
+                Comment
+    */
+
+    int x;
+    x = 25;
+
+    char gen = 'M';
+    float per = 78.4;
+
+    printf("My age is %d\n",x);
+    printf("My gen is %c\n",gen);
+}
